5b71965790c7176e44d2419c7799c51d4d87b72e
Isuru Gunarathne <isuru623@gmail.com>
2023-08-17 10:03:49 +0530

    Delete placeholders and refactor

--- Aether/navigation/MainContainer.js
+++ Aether/navigation/MainContainer.js
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // Screens
-import HomeScreen from '../components/pages/HomeScreen';
-import MyFlightsScreen from '../components/pages/MyFlightsScreen';
-import ExploreScreen from '../components/pages/ExploreScreen';
-import ProfileScreen from '../components/pages/ProfileScreen';
-import PersonalInfoScreen from '../components/pages/PersonalInfoScreen';
-import ChoosePod from '../components/pages/ChoosePod';
+import HomeScreen from "../components/pages/HomeScreen";
+import MyFlightsScreen from "../components/pages/MyFlightsScreen";
+import ExploreScreen from "../components/pages/ExploreScreen";
+import ProfileScreen from "../components/pages/ProfileScreen";
+import PersonalInfoScreen from "../components/pages/PersonalInfoScreen";
+import ChoosePod from "../components/pages/ChoosePod";
 
 //Screen names
 const homeName = "Home";
@@ -24,68 +24,70 @@ const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const MainTabNavigator = () => (
-    <Tab.Navigator
-                initialRouteName={homeName}
+  <Tab.Navigator
+    initialRouteName={homeName}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        let rn = route.name;
 
-                screenOptions={({ route }) => (
+        if (rn === homeName) {
+          iconName = focused ? "home" : "home-outline";
+        } else if (rn === FlightsName) {
+          iconName = focused ? "paper-plane" : "paper-plane-outline";
+        } else if (rn === ExploreName) {
+          iconName = focused ? "compass-sharp" : "compass-outline";
+        } else if (rn === profileName) {
+          iconName = focused ? "person" : "person-outline";
+        }
 
-                    {
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName;
-                            let rn = route.name;
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
 
-                            if (rn === homeName) {
-                                iconName = focused ? 'home' : 'home-outline';
-
-                            } else if (rn === FlightsName) {
-                                iconName = focused ? 'paper-plane' : 'paper-plane-outline';
-
-                            } else if (rn === ExploreName) {
-                                iconName = focused ? 'compass-sharp' : 'compass-outline';
-
-                            } else if (rn === profileName) {
-                                iconName = focused ? 'person' : 'person-outline';
-                            }
-
-                            // You can return any component that you like here!
-                            return <Ionicons name={iconName} size={size} color={color} />;
-                        },
-
-                        tabBarActiveTintColor: "tomato",
-                        tabBarInactiveTintColor: "grey",
-                        tabBarLabelStyle: {
-                            paddingBottom: 10,
-                            fontSize: 10
-                        },
-                        tabBarStyle: [
-                            {
-                                display: "flex"
-                            },
-                            null
-                        ]
-                    }
-                )
-                }>
-
-                <Tab.Screen name={homeName} component={HomeScreen} />
-                <Tab.Screen name={FlightsName} component={MyFlightsScreen} />
-                <Tab.Screen name={ExploreName} component={ExploreScreen} />
-                <Tab.Screen name={profileName} component={ProfileScreen} />
-
-            </Tab.Navigator>
-  );
-
+      tabBarActiveTintColor: "tomato",
+      tabBarInactiveTintColor: "grey",
+      tabBarLabelStyle: {
+        paddingBottom: 10,
+        fontSize: 10,
+      },
+      tabBarStyle: [
+        {
+          display: "flex",
+        },
+        null,
+      ],
+    })}
+  >
+    <Tab.Screen name={homeName} component={HomeScreen} />
+    <Tab.Screen name={FlightsName} component={MyFlightsScreen} />
+    <Tab.Screen name={ExploreName} component={ExploreScreen} />
+    <Tab.Screen name={profileName} component={ProfileScreen} />
+  </Tab.Navigator>
+);
 
 function MainContainer() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-      <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name={personalInfoName} component={PersonalInfoScreen} options={{ tabBarVisible: false }} />
-      <Stack.Screen name={choosePodName} component={ChoosePod} options={{ tabBarVisible: false }} />
-    </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name={personalInfoName}
+          component={PersonalInfoScreen}
+          options={{ tabBarVisible: false }}
+        />
+        <Stack.Screen
+          name={choosePodName}
+          component={ChoosePod}
+          options={{ tabBarVisible: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default MainContainer;
