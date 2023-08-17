05310ca1dbcadb8eb1656aa0b2b63f178a117a78
Isuru Gunarathne <isuru623@gmail.com>
2023-08-17 12:43:47 +0530

    fix small bug in navigation and screens

--- Aether/navigation/MainContainer.js
+++ Aether/navigation/MainContainer.js
@@ -14,13 +14,14 @@ import MyFlightsScreen from "../components/pages/MyFlightsScreen";
 import ExploreScreen from "../components/pages/ExploreScreen";
 import ProfileScreen from "../components/pages/ProfileScreen";
 import PersonalInfoScreen from "../components/pages/PersonalInfoScreen";
+import ChoosePodScreen from "../components/pages/ChoosePodScreen";
 
 // Theming
 import { DarkTheme as NavigationDarkTheme } from "@react-navigation/native";
 import {
-	MD3DarkTheme,
-	adaptNavigationTheme,
-	PaperProvider,
+  MD3DarkTheme,
+  adaptNavigationTheme,
+  PaperProvider,
 } from "react-native-paper";
 import merge from "deepmerge";
 
@@ -34,14 +35,14 @@ const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const { DarkTheme } = adaptNavigationTheme({
-	reactNavigationDark: NavigationDarkTheme,
+  reactNavigationDark: NavigationDarkTheme,
 });
 
 const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 const theme = {
-	...CombinedDarkTheme,
-	colors: customPallete.colors, // Copy it from the color codes scheme and then use it here
+  ...CombinedDarkTheme,
+  colors: customPallete.colors, // Copy it from the color codes scheme and then use it here
 };
 
 //Screen names
@@ -52,123 +53,126 @@ const profileName = "Profile";
 const personalInfoName = "Personal Info";
 const choosePodName = "Choose Pod";
 
-
-
 const MainTabNavigator = () => (
-	<Tab.Navigator
-		initialRouteName={homeName}
-		screenOptions={({ route }) => ({
-			header: (props) => <CustomNavigationBar {...props} />,
-		})}
-		tabBar={({ navigation, state, descriptors, insets }) => (
-			<BottomNavigation.Bar
-				navigationState={state}
-				safeAreaInsets={insets}
-				onTabPress={({ route, preventDefault }) => {
-					const event = navigation.emit({
-						type: "tabPress",
-						target: route.key,
-						canPreventDefault: true,
-					});
+  <Tab.Navigator
+    initialRouteName={homeName}
+    screenOptions={({ route }) => ({
+      header: (props) => <CustomNavigationBar {...props} />,
+    })}
+    tabBar={({ navigation, state, descriptors, insets }) => (
+      <BottomNavigation.Bar
+        navigationState={state}
+        safeAreaInsets={insets}
+        onTabPress={({ route, preventDefault }) => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
 
-					if (event.defaultPrevented) {
-						preventDefault();
-					} else {
-						navigation.dispatch({
-							...CommonActions.navigate(route.name, route.params),
-							target: state.key,
-						});
-					}
-				}}
-				renderIcon={({ route, focused, color }) => {
-					const { options } = descriptors[route.key];
-					if (options.tabBarIcon) {
-						return options.tabBarIcon({ focused, color, size: 24 });
-					}
+          if (event.defaultPrevented) {
+            preventDefault();
+          } else {
+            navigation.dispatch({
+              ...CommonActions.navigate(route.name, route.params),
+              target: state.key,
+            });
+          }
+        }}
+        renderIcon={({ route, focused, color }) => {
+          const { options } = descriptors[route.key];
+          if (options.tabBarIcon) {
+            return options.tabBarIcon({ focused, color, size: 24 });
+          }
 
-					return null;
-				}}
-				getLabelText={({ route }) => {
-					const { options } = descriptors[route.key];
-					const label =
-						options.tabBarLabel !== undefined
-							? options.tabBarLabel
-							: options.title !== undefined
-							? options.title
-							: route.title;
+          return null;
+        }}
+        getLabelText={({ route }) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.title;
 
-					return label;
-				}}
-			/>
-		)}
-	>
-		<Tab.Screen
-			name={homeName}
-			component={HomeScreen}
-			options={{
-				tabBarLabel: homeName,
-				tabBarIcon: ({ color, size }) => {
-					return <Icon name="home-outline" size={size} color={color} />;
-				},
-			}}
-		/>
-		<Tab.Screen
-			name={FlightsName}
-			component={MyFlightsScreen}
-			options={{
-				tabBarLabel: FlightsName,
-				tabBarIcon: ({ color, size }) => {
-					return <Icon name="paper-plane-outline" size={size} color={color} />;
-				},
-			}}
-		/>
-		<Tab.Screen
-			name={ExploreName}
-			component={ExploreScreen}
-			options={{
-				tabBarLabel: ExploreName,
-				tabBarIcon: ({ color, size }) => {
-					return <Icon name="compass-outline" size={size} color={color} />;
-				},
-			}}
-		/>
-		<Tab.Screen
-			name={profileName}
-			component={ProfileScreen}
-			options={{
-				tabBarLabel: profileName,
-				tabBarIcon: ({ color, size }) => {
-					return <Icon name="person-outline" size={size} color={color} />;
-				},
-			}}
-		/>
-	</Tab.Navigator>
+          return label;
+        }}
+      />
+    )}
+  >
+    <Tab.Screen
+      name={homeName}
+      component={HomeScreen}
+      options={{
+        tabBarLabel: homeName,
+        tabBarIcon: ({ color, size }) => {
+          return <Icon name="home-outline" size={size} color={color} />;
+        },
+      }}
+    />
+    <Tab.Screen
+      name={FlightsName}
+      component={MyFlightsScreen}
+      options={{
+        tabBarLabel: FlightsName,
+        tabBarIcon: ({ color, size }) => {
+          return <Icon name="paper-plane-outline" size={size} color={color} />;
+        },
+      }}
+    />
+    <Tab.Screen
+      name={ExploreName}
+      component={ExploreScreen}
+      options={{
+        tabBarLabel: ExploreName,
+        tabBarIcon: ({ color, size }) => {
+          return <Icon name="compass-outline" size={size} color={color} />;
+        },
+      }}
+    />
+    <Tab.Screen
+      name={profileName}
+      component={ProfileScreen}
+      options={{
+        tabBarLabel: profileName,
+        tabBarIcon: ({ color, size }) => {
+          return <Icon name="person-outline" size={size} color={color} />;
+        },
+      }}
+    />
+  </Tab.Navigator>
 );
 
 function MainContainer() {
-	return (
-		<PaperProvider theme={theme}>
-			<NavigationContainer theme={theme}>
-				<Stack.Navigator
-					initialRouteName="Main"
-					screenOptions={{
-						header: (props) => <CustomNavigationBar {...props} />,
-					}}
-				>
-					<Stack.Screen
-						name="Main"
-						component={MainTabNavigator}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name={personalInfoName}
-						component={PersonalInfoScreen}
-						options={{ tabBarVisible: false }}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</PaperProvider>
-	);
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator
+          initialRouteName="Main"
+          screenOptions={{
+            header: (props) => <CustomNavigationBar {...props} />,
+          }}
+        >
+          <Stack.Screen
+            name="Main"
+            component={MainTabNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name={personalInfoName}
+            component={PersonalInfoScreen}
+            options={{ tabBarVisible: false }}
+          />
+          <Stack.Screen
+            name={choosePodName}
+            component={ChoosePodScreen}
+            options={{ tabBarVisible: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
 }
 
 export default MainContainer;
