d30719c7901148bc34d065de4da73c9589609df5
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-20 00:39:01 +0530

    Add gradient bg with stars to DynamicBackground

--- Aether/components/templates/DynamicBackground.js
+++ Aether/components/templates/DynamicBackground.js
@@ -1,10 +1,33 @@
 import { useBackground } from "../../context/BackgroundContext";
-import { ImageBackground, StyleSheet, Dimensions } from "react-native";
+import {
+	ImageBackground,
+	StyleSheet,
+	Dimensions,
+	Text,
+	View,
+	Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import stars from "../../assets/images/stars.png";
 
-const DynamicBackground = ({ children, ...props }) => {
+const DynamicBackground = ({ children, staticBG = false }) => {
 	const { backgroundColor, backgroundImage, setBackground } = useBackground();
+	// (120deg, #001B2F 0.14%, #001125 7.00%, #000 28.86%, #000 99.98%, #443045 99.99%, #16151A 100%));
+	const gradientBG = (
+		<View style={styles.container}>
+			<LinearGradient
+				colors={["#001B2F", "#001125", "#000"]}
+				style={styles.gradient}
+			/>
 
-	return (
+			<View style={styles.starContainer}>
+				<Image style={styles.starImage} source={stars} />
+			</View>
+			{children}
+		</View>
+	);
+
+	const imageBG = (
 		<ImageBackground
 			style={styles.imageContainer}
 			imageStyle={styles.image}
@@ -13,6 +36,8 @@ const DynamicBackground = ({ children, ...props }) => {
 			{children}
 		</ImageBackground>
 	);
+
+	return staticBG ? gradientBG : imageBG;
 };
 
 // Get screen heihgt and width
@@ -30,6 +55,29 @@ const styles = StyleSheet.create({
 		justifyContent: "flex-end",
 		borderRadius: 8,
 		paddingTop: (15 / 100) * height,
+	},
+	container: {
+		flex: 1,
+		justifyContent: "flex-end",
+		backgroundColor: "black",
+		paddingTop: (15 / 100) * height,
+	},
+	gradient: {
+		position: "absolute",
+		left: 0,
+		right: 0,
+		top: 0,
+		height: 300,
+	},
+	starContainer: {
+		...StyleSheet.absoluteFillObject,
+		flex: 1,
+		justifyContent: "flex-end",
+	},
+	starImage: {
+		flex: 1,
+		width: "100%",
+		opacity: 0.35,
 	},
 });
 
