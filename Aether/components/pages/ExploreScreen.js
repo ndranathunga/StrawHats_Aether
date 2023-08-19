337bdb1109375c729a6d217b4c4c99888b7daa06
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-19 14:08:39 +0530

    Add dynamic background to Profile and Explore screens

--- Aether/components/pages/ExploreScreen.js
+++ Aether/components/pages/ExploreScreen.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import DynamicBackground from "../templates/DynamicBackground";
 
 export default function ExploreScreen({ navigation }) {
 	return (
-		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text
-				onPress={() => navigation.navigate("Home")}
-				style={{ fontSize: 26, fontWeight: "bold" }}
-			>
-				Explore Screen
-			</Text>
-		</View>
+		<DynamicBackground>
+			<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+				<Text
+					onPress={() => navigation.navigate("Home")}
+					style={{ fontSize: 26, fontWeight: "bold" }}
+				>
+					Explore Screen
+				</Text>
+			</View>
+		</DynamicBackground>
 	);
 }
