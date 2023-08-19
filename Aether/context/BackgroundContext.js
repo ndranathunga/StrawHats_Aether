a62d89b24ba9f9782ac76cb92376c009833bef54
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-19 14:05:53 +0530

    Add image list to background context

--- Aether/context/BackgroundContext.js
+++ Aether/context/BackgroundContext.js
@@ -15,11 +15,16 @@ const items = [
 		id: "002",
 		image: require("../assets/images/backgrounds/mars-bg-temp.jpeg"),
 	},
+	{
+		id: "003",
+		image: require("../assets/images/backgrounds/black-hole-bg-temp.jpeg"),
+	},
 ];
 
 export const BackgroundProvider = ({ children }) => {
 	const [backgroundColor, setBackgroundColor] = useState("black");
 	const [backgroundImage, setBackgroundImage] = useState(items[0].image);
+	const [imageList, setImageList] = useState(items);
 
 	const setBackground = (color, image) => {
 		setBackgroundColor(color);
@@ -28,7 +33,7 @@ export const BackgroundProvider = ({ children }) => {
 
 	return (
 		<BackgroundContext.Provider
-			value={{ backgroundColor, backgroundImage, setBackground }}
+			value={{ backgroundColor, backgroundImage, setBackground, imageList }}
 		>
 			{children}
 		</BackgroundContext.Provider>
