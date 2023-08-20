b875ca085ecd918dcbe287c9372e86d23fe111f8
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-20 19:13:34 +0530

    Whole bunch of tweaks

--- Aether/components/pages/DatePickerScreen.js
+++ Aether/components/pages/DatePickerScreen.js
@@ -12,139 +12,159 @@ import DynamicBackground from "../templates/DynamicBackground";
 import { defultBoxStyle } from "../atoms/Styles/defultBoxStyle";
 
 export default function DatePickerScreen() {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  const [selectedStartDate, setSelectedStartDate] = useState(null);
-  const [selectedReturnDate, setSelectedReturnDate] = useState(null);
-  const [showReturnDatePicker, setShowReturnDatePicker] = useState(false);
+	const [selectedStartDate, setSelectedStartDate] = useState(null);
+	const [selectedReturnDate, setSelectedReturnDate] = useState(null);
+	const [showReturnDatePicker, setShowReturnDatePicker] = useState(true);
 
-  const onDateChange = (date, type) => {
-    if (type === "START_DATE") {
-      setSelectedStartDate(date);
-      if (selectedReturnDate && selectedReturnDate.isBefore(date, "day")) {
-        setSelectedReturnDate(null);
-      }
-    } else if (type === "RETURN_DATE") {
-      setSelectedReturnDate(date);
-    }
-  };
+	const onDateChange = (date, type) => {
+		if (type === "START_DATE") {
+			setSelectedStartDate(date);
+			if (selectedReturnDate && selectedReturnDate.isBefore(date, "day")) {
+				setSelectedReturnDate(null);
+			}
+		} else if (type === "RETURN_DATE") {
+			setSelectedReturnDate(date);
+		}
+	};
 
-  const toggleReturnDatePicker = () => {
-    if (showReturnDatePicker) {
-      setSelectedReturnDate(null); // Reset the return date when calendar is hidden
-    }
-    setShowReturnDatePicker(!showReturnDatePicker);
-  };
+	const toggleReturnDatePicker = () => {
+		if (showReturnDatePicker) {
+			setSelectedReturnDate(null); // Reset the return date when calendar is hidden
+		}
+		setShowReturnDatePicker(!showReturnDatePicker);
+	};
 
-  const handleSelectPress = () => {
-    // TODO: Implement saving selectedStartDate and selectedReturnDate to the database
-    navigation.navigate("Select Passengers");
-  };
+	const handleSelectPress = () => {
+		// TODO: Implement saving selectedStartDate and selectedReturnDate to the database
+		navigation.navigate("Select Passengers");
+	};
 
-  const formatDate = (date) => {
-    if (date) {
-      const formattedDate = date.format("DD MMM YYYY");
-      return formattedDate;
-    }
-    return "Select Date";
-  };
+	const formatDate = (date) => {
+		if (date) {
+			const formattedDate = date.format("DD MMM YYYY");
+			return formattedDate;
+		}
+		return "Select Date";
+	};
 
-  const isDateDisabled = (date, type) => {
-    const today = moment();
-    if (type === "START_DATE" && selectedReturnDate) {
-      return (
-        date.isAfter(selectedReturnDate, "day") || date.isBefore(today, "day")
-      );
-    } else if (type === "RETURN_DATE" && selectedStartDate) {
-      return (
-        date.isBefore(selectedStartDate, "day") || date.isBefore(today, "day")
-      );
-    }
-    return date.isBefore(today, "day");
-  };
+	const isDateDisabled = (date, type) => {
+		const today = moment();
+		if (type === "START_DATE" && selectedReturnDate) {
+			return (
+				date.isAfter(selectedReturnDate, "day") || date.isBefore(today, "day")
+			);
+		} else if (type === "RETURN_DATE" && selectedStartDate) {
+			return (
+				date.isBefore(selectedStartDate, "day") || date.isBefore(today, "day")
+			);
+		}
+		return date.isBefore(today, "day");
+	};
 
-  return (
-    <DynamicBackground>
-      <CenterViewContainer style={{ padding: 20 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: 10,
-          }}
-        >
-          <SelectedDateCard
-            title={"Departure"}
-            content={
-              selectedStartDate ? formatDate(selectedStartDate) : "Select Date"
-            }
-          />
+	return (
+		<DynamicBackground>
+			<CenterViewContainer>
+				<View
+					style={{
+						flexDirection: "row",
+						alignItems: "center",
+						justifyContent: "center",
+						paddingHorizontal: 40,
+						paddingBottom: 10,
+					}}
+				>
+					<SelectedDateCard
+						title={"Departure"}
+						content={
+							selectedStartDate ? formatDate(selectedStartDate) : "Select Date"
+						}
+					/>
 
-          <SelectedDateCard
-            onPress={toggleReturnDatePicker}
-            title={"Return"}
-            content={
-              selectedReturnDate
-                ? formatDate(selectedReturnDate)
-                : "Select Date"
-            }
-          />
-        </View>
+					<SelectedDateCard
+						onPress={toggleReturnDatePicker}
+						title={"Return"}
+						content={
+							selectedReturnDate
+								? formatDate(selectedReturnDate)
+								: "Select Date"
+						}
+					/>
+				</View>
 
-        <View style={{ flex: 1 }}>
-          <View style={{ flex: 0.5 }}>
-            <View
-              style={[
-                defultBoxStyle.container,
-                defultBoxStyle.sizes,
-                { marginTop: 1, marginBottom: 5 },
-              ]}
-            >
-              <CalendarPicker
-                width={280}
-                selectedDayColor={"#84BCFF"}
-                textStyle={[textStyles.title3, { color: "white" }]}
-                onDateChange={(date) => onDateChange(date, "START_DATE")}
-                disabledDates={(date) => isDateDisabled(date, "START_DATE")}
-              />
-            </View>
-          </View>
+				<View style={{ flex: 1 }}>
+					<View style={{ flex: 0.45 }}>
+						<View
+							style={[
+								defultBoxStyle.container,
+								defultBoxStyle.sizes,
+								{ marginTop: 1, marginBottom: 5 },
+							]}
+						>
+							<CalendarPicker
+								width={280}
+								selectedDayColor={"#84BCFF"}
+								textStyle={[textStyles.title3, { color: "white" }]}
+								onDateChange={(date) => onDateChange(date, "START_DATE")}
+								disabledDates={(date) => isDateDisabled(date, "START_DATE")}
+							/>
+						</View>
+					</View>
 
-          <View style={{ flex: 0.5 }}>
-            {showReturnDatePicker && (
-              <View
-                style={[
-                  defultBoxStyle.container,
-                  defultBoxStyle.sizes,
-                  { marginTop: 1, marginBottom: 5 },
-                ]}
-              >
-                <CalendarPicker
-                  width={280}
-                  selectedDayColor={"#84BCFF"}
-                  textStyle={[textStyles.title3, { color: "white" }]}
-                  onDateChange={(date) => onDateChange(date, "RETURN_DATE")}
-                  disabledDates={(date) => isDateDisabled(date, "RETURN_DATE")}
-                />
-              </View>
-            )}
-          </View>
-        </View>
-
-        <CustomButton onPress={handleSelectPress} title={"Select"} />
-      </CenterViewContainer>
-    </DynamicBackground>
-  );
+					<View style={{ flex: 0.45 }}>
+						{showReturnDatePicker && (
+							<View
+								style={[
+									defultBoxStyle.container,
+									defultBoxStyle.sizes,
+									{ marginTop: 1, marginBottom: 5 },
+								]}
+							>
+								<CalendarPicker
+									width={280}
+									selectedDayColor={"#84BCFF"}
+									textStyle={[textStyles.title3, { color: "white" }]}
+									onDateChange={(date) => onDateChange(date, "RETURN_DATE")}
+									disabledDates={(date) => isDateDisabled(date, "RETURN_DATE")}
+								/>
+							</View>
+						)}
+					</View>
+					<View
+						style={{
+							alignContent: "center",
+							justifyContent: "center",
+							alignItems: "center",
+							marginTop: 10,
+						}}
+					>
+						<CustomButton
+							style={styles.btn}
+							onPress={handleSelectPress}
+							title={"Select"}
+						/>
+					</View>
+				</View>
+			</CenterViewContainer>
+		</DynamicBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  iconContainer: {
-    position: "absolute",
-    left: 0,
-    top: "50%",
-    transform: [{ translateY: -8 }],
-    paddingHorizontal: 10,
-    flex: 1,
-  },
+	iconContainer: {
+		position: "absolute",
+		left: 0,
+		top: "50%",
+		transform: [{ translateY: -8 }],
+		paddingHorizontal: 10,
+		flex: 1,
+	},
+	btn: {
+		// width: "90%",
+		// alignContent: "center",
+		// justifyContent: "center",
+		marginTop: 20,
+		marginBottom: 50,
+	},
 });
