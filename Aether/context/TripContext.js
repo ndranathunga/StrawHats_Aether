5c5cf2b25e9adb2fc6947a909106c5628734a565
Isuru Gunarathne <isuru623@gmail.com>
2023-08-20 16:53:59 +0530

    Trying to put context

--- Aether/context/TripContext.js
+++ Aether/context/TripContext.js
@@ -1,28 +1,67 @@
 import React, { createContext, useContext, useState } from "react";
 
+// Create the context
 const TripContext = createContext();
 
-export const useTrip = () => {
-	return useContext(TripContext);
+// Define the default values
+const defaultTripValues = {
+  startLocation: "",
+  endLocation: "",
+  departureDate: "",
+  arrivalDate: "",
+  passengers: 0,
 };
 
+// Create a custom hook to access the TripContext
+export function useTripContext() {
+  return useContext(TripContext);
+}
 
+// Create the TripProvider component
+export function TripProvider({ children }) {
+  const [tripData, setTripData] = useState(defaultTripValues);
 
-export const TripProvider = ({ children }) => {
-	const [backgroundColor, setTripColor] = useState("black");
-	const [backgroundImage, setTripImage] = useState(items[0].image);
-	const [imageList, setImageList] = useState(items);
+  const updateStartLocation = (newStartLocation) => {
+    setTripData((prevTripData) => ({
+      ...prevTripData,
+      startLocation: newStartLocation,
+    }));
+  };
 
-	const setTrip = (color, image) => {
-		setTripColor(color);
-		setTripImage(image);
-	};
+  const updateEndLocation = (newEndLocation) => {
+    setTripData((prevTripData) => ({
+      ...prevTripData,
+      endLocation: newEndLocation,
+    }));
+  };
+  const updateDepartureDate = (newDepartureDate) => {
+    setTripData((prevTripData) => ({
+      ...prevTripData,
+      departureDate: newDepartureDate,
+    }));
+  };
+  const updateArrivalDate = (newArrivalDate) => {
+    setTripData((prevTripData) => ({
+      ...prevTripData,
+      arrivalDate: newArrivalDate,
+    }));
+  };
 
-	return (
-		<TripContext.Provider
-			value={{ backgroundColor, backgroundImage, setTrip, imageList }}
-		>
-			{children}
-		</TripContext.Provider>
-	);
-};
+  const updatePassengers = (newPassengers) => {
+    setTripData((prevTripData) => ({
+      ...prevTripData,
+      passengers: newPassengers,
+    }));
+  };
+
+  const tripContextValue = {
+    ...defaultTripValues,
+    // You can add other methods or state here if needed
+  };
+
+  return (
+    <TripContext.Provider value={tripContextValue}>
+      {children}
+    </TripContext.Provider>
+  );
+}
