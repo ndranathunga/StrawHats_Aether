c3bed974ac71d6449f1c55fbc1ca6b60341792d1
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-20 14:06:41 +0530

    Update help screen

--- Aether/components/templates/component/SlideItem.js
+++ Aether/components/templates/component/SlideItem.js
@@ -6,15 +6,21 @@ import {
 	Dimensions,
 	Animated,
 	Easing,
+	ImageBackground,
 } from "react-native";
 import React from "react";
 import Body1 from "../../atoms/text/Body1";
 import Title1 from "../../atoms/text/Title1";
+import logo from "../../../assets/images/text-logo.png";
+import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 const { width, height } = Dimensions.get("screen");
 
 const SlideItem = ({ item }) => {
 	const translateYImage = new Animated.Value(40);
+	const itemID = item.id;
+	const navigation = useNavigation();
 
 	Animated.timing(translateYImage, {
 		toValue: 0,
@@ -24,13 +30,50 @@ const SlideItem = ({ item }) => {
 	}).start();
 
 	return (
-		<View style={styles.container}>
-			<Image source={item.img} resizeMode="contain" style={styles.image} />
-			<View style={styles.content}>
-				<Title1 style={styles.title}>{item.title}</Title1>
-				<Body1 style={styles.description}>{item.description}</Body1>
+		<ImageBackground
+			source={item.img}
+			resizeMode="cover"
+			imageStyle={styles.image}
+		>
+			<View style={styles.container}>
+				<View
+					style={{
+						flex: 0.5,
+						alignItems: "flex-start",
+						justifyContent: "flex-start",
+					}}
+				>
+					<Image
+						source={logo}
+						style={{
+							display: "block",
+							position: "absolute",
+							top: 80,
+							left: 30,
+						}}
+					></Image>
+					<Button
+						mode="contained"
+						onPress={() => {
+							navigation.navigate("Home");
+						}}
+						style={{
+							display: "block",
+							position: "absolute",
+							top: 65,
+							right: 30,
+							backgroundColor: "white",
+						}}
+					>
+						{itemID === 4 ? "Get Started >" : "Skip"}
+					</Button>
+				</View>
+				<View style={styles.content}>
+					<Title1 style={styles.title}>{item.title}</Title1>
+					<Body1 style={styles.description}>{item.description}</Body1>
+				</View>
 			</View>
-		</View>
+		</ImageBackground>
 	);
 };
 
@@ -40,15 +83,18 @@ const styles = StyleSheet.create({
 	container: {
 		width,
 		height,
-		alignItems: "center",
+		// alignItems: "center",
+		flexDirection: "column",
 	},
 	image: {
-		flex: 0.7,
-		width: "100%",
+		// flex: 0.7,
+		// width: "100%",
+		opacity: 0.8,
 	},
 	content: {
-		flex: 0.3,
+		flex: 0.5,
 		alignItems: "center",
+		justifyContent: "center",
 	},
 	title: {
 		fontSize: 24,
--- Aether/components/templates/data/index.js
+++ Aether/components/templates/data/index.js
@@ -1,7 +1,7 @@
 export default [
 	{
 		id: 1,
-		img: require("../assets/watch7.jpeg"),
+		img: require("../assets/1.png"),
 		title: "Welcome to Aether",
 		description:
 			"Your cosmic journey begins! Explore the universe and book interplanetary travels with ease using Aether. Get ready to embark on a stellar adventure like no other.",
@@ -9,7 +9,7 @@ export default [
 	},
 	{
 		id: 2,
-		img: require("../assets/airpod.jpeg"),
+		img: require("../assets/2.png"),
 		title: "Discover New Horizons",
 		description:
 			" Unveil breathtaking destinations as you scroll through our celestial offerings. From Mars to distant galaxies, Aether opens doors to endless explorations.",
@@ -17,7 +17,7 @@ export default [
 	},
 	{
 		id: 3,
-		img: require("../assets/airpodmax.jpeg"),
+		img: require("../assets/3.png"),
 		title: "Planetary Insights",
 		description:
 			"Dive into the wonders of each planet through vivid descriptions and stunning visuals. Get a sneak peek of what awaits on your cosmic travels.",
@@ -25,7 +25,7 @@ export default [
 	},
 	{
 		id: 4,
-		img: require("../assets/charger.png"),
+		img: require("../assets/4.png"),
 		title: "Your Cosmic Guide",
 		description:
 			"Experience seamless booking and expert advice at your fingertips. Let Aether be your guide, turning your dreams of space travel into reality.",
