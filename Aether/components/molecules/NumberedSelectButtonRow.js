854d67775fc0e5b22b38c6f6c62e69aa1127f0f3
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-08-20 11:08:00 +0530

    Trying to fix this :(

--- Aether/components/molecules/NumberedSelectButtonRow.js
+++ Aether/components/molecules/NumberedSelectButtonRow.js
@@ -3,38 +3,38 @@ import { View, StyleSheet } from "react-native";
 import PassengerButton from "../atoms/buttons/PassengerButton";
 
 export default function NumberedSelectButtonRow({
-  handlePress,
-  selectedButton, // Change selected to selectedButton
-  ButtonNumbers,
-  numberOfButtons,
+	handlePress,
+	selectedButton, // Change selected to selectedButton
+	ButtonNumbers,
+	numberOfButtons,
 }) {
-  // Limit the number of buttons to a maximum of 10
-  const buttonCount = Math.min(numberOfButtons, 10);
+	// Limit the number of buttons to a maximum of 10
+	const buttonCount = Math.min(numberOfButtons, 10);
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.seatRow}>
-        {ButtonNumbers.slice(0, buttonCount).map((buttonNumber) => (
-          <PassengerButton
-            key={buttonNumber}
-            seatNumber={buttonNumber}
-            isSelected={selectedButton === buttonNumber}
-            onPress={handlePress} // No need to change here
-          />
-        ))}
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.seatRow}>
+				{ButtonNumbers.slice(0, buttonCount).map((buttonNumber) => (
+					<PassengerButton
+						key={buttonNumber}
+						seatNumber={buttonNumber}
+						isSelected={selectedButton === buttonNumber}
+						onPress={handlePress} // No need to change here
+					/>
+				))}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
-    flex: 1,
-  },
-  seatRow: {
-    flexDirection: "row",
-  },
+	container: {
+		// justifyContent: "center",
+		alignItems: "center",
+		// padding: 16,
+		flex: 1,
+	},
+	seatRow: {
+		flexDirection: "row",
+	},
 });
--- Aether/components/pages/SelectPassengersScreen.js
+++ Aether/components/pages/SelectPassengersScreen.js
@@ -1,90 +1,98 @@
-import React from "react";
+import { useState } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import DynamicBackground from "../templates/DynamicBackground";
 import CenterViewContainer from "../atoms/view-containers/CenterViewContainer";
 import NumberedSelectButtonRow from "../molecules/NumberedSelectButtonRow";
 import textStyles from "../atoms/text/Styles";
 import Body2 from "../atoms/text/Body2";
+import Title4 from "../atoms/text/Title4";
 
 export default function ExploreScreen({ navigation }) {
-  const [selectedButton, setSelectedButton] = React.useState(null);
+	const [selectedButton, setSelectedButton] = useState(null);
+	const [adults, setAdults] = useState(1);
+	const [children, setChildren] = useState(0);
+	const [infants, setInfants] = useState(0);
 
-  const handleButtonPress = (buttonNumber) => {
-    setSelectedButton(buttonNumber);
-  };
+	const handleButtonPress = (buttonNumber) => {
+		setSelectedButton(buttonNumber);
+	};
 
-  return (
-    <DynamicBackground>
-        <CenterViewContainer>
-        <View style={styles.container}>
-          <View style={styles.textContainer}>
-            <View>
-              <Text style={[textStyles.body2, { color: "white" ,textAlign:"left"}]}>Adults</Text>
-            </View>
-            <View >
-              <Text style={[textStyles.body2, { color: "white" }]}>
-                12 years or above
-              </Text>
-            </View>
-          </View>
-          <NumberedSelectButtonRow
-            handlePress={handleButtonPress}
-            selectedButton={selectedButton}
-            ButtonNumbers={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-            numberOfButtons={10}
-          />
-          </View>
+	return (
+		<DynamicBackground>
+			<CenterViewContainer>
+				<View style={{ flex: 1, flexDirection: "column" }}>
+					<View style={styles.container}>
+						<View style={styles.textContainer}>
+							<View style={{ flex: 3 }}>
+								<Title4>Adults</Title4>
+							</View>
+							<View style={{ flex: 3, alignItems: "flex-end" }}>
+								<Body2>12 years or above</Body2>
+							</View>
+						</View>
+						<NumberedSelectButtonRow
+							handlePress={(buttonNumber) => {
+								setAdults(buttonNumber);
+							}}
+							selectedButton={adults}
+							ButtonNumbers={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+							numberOfButtons={10}
+						/>
+					</View>
 
-          <View style={styles.container}>
-          <View style={styles.textContainer}>
-            <View>
-              <Text style={[textStyles.body2, { color: "white" ,textAlign:"left"}]}>Children</Text>
-            </View>
-            <View >
-              <Text style={[textStyles.body2, { color: "white" }]}>
-              2 -12 yr
-              </Text>
-            </View>
-          </View>
-          <NumberedSelectButtonRow
-            handlePress={handleButtonPress}
-            selectedButton={selectedButton}
-            ButtonNumbers={[1, 2, 3, 4, 5, 6, 7, 8]}
-            numberOfButtons={10}
-          />
-          </View>
+					<View style={styles.container}>
+						<View style={styles.textContainer}>
+							<View style={{ flex: 3 }}>
+								<Title4>Children</Title4>
+							</View>
+							<View style={{ flex: 3, alignItems: "flex-end" }}>
+								<Body2>2 -12 years</Body2>
+							</View>
+						</View>
+						<NumberedSelectButtonRow
+							handlePress={(buttonNumber) => {
+								setAdults(buttonNumber);
+							}}
+							selectedButton={adults}
+							ButtonNumbers={[0, 1, 2, 3, 4, 5, 6, 7, 8]}
+							numberOfButtons={10}
+						/>
+					</View>
 
-          <View style={styles.container}>
-          <View style={styles.textContainer}>
-              <Text style={[textStyles.body2, { color: "white" }]}>Infants</Text>
-              <Text style={[textStyles.body2, { color: "white" }]}>0 - 2 yr</Text>
-              
-          </View>
-          
-          <NumberedSelectButtonRow
-            handlePress={handleButtonPress}
-            selectedButton={selectedButton}
-            ButtonNumbers={[1, 2, 3, 4]}
-            numberOfButtons={10}
-          />
-          </View>
-        </CenterViewContainer>
-    </DynamicBackground>
-  );
+					{/* <View style={styles.container}>
+						<View style={styles.textContainer}>
+							<View style={{ flex: 3 }}>
+								<Title4>Infants</Title4>
+							</View>
+							<View style={{ flex: 3, alignItems: "flex-end" }}>
+								<Body2>0 - 2 years</Body2>
+							</View>
+						</View>
+						<NumberedSelectButtonRow
+							handlePress={(buttonNumber) => {
+								setAdults(buttonNumber);
+							}}
+							selectedButton={adults}
+							ButtonNumbers={[0, 1, 2, 3, 4]}
+							numberOfButtons={10}
+						/>
+					</View> */}
+				</View>
+			</CenterViewContainer>
+		</DynamicBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, // Takes up full height
-  },
-  textContainer: {
-    flexDirection: "row",
-    flex: 1,
-
-    alignItems: "center",
-    marginBottom: 10,
-    width: "100%",
-    backgroundColor: "#1E1E1E",
-  },
-
+	container: {
+		// flex: 3,
+		marginTop: 0,
+		// flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	textContainer: {
+		flexDirection: "row",
+		marginBottom: 15,
+	},
 });
